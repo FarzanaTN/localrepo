63878b203d009f95b83985dbbe2fc2cff2115c19
FarzanaTN <nishatfarzana14@gmail.com>
2025-03-16 14:38:20 +0600

    in feature1 change

--- index.c
+++ index.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 
+int testing(){
+    printf("Do you know how to do it?");
+    return 1;
+}
+
 int main(){
 
     printf("INDEX\n");
     printf("This is a new feature (dropdown) \n");
     printf("This is a new feature (button) \n");
+    printf("adding in feature1 branch");
 
     return 0;
 }
