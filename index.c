9cde65ee79c18f31bd8618f362cc108e2bd9c1c7
FarzanaTN <nishatfarzana14@gmail.com>
2025-03-16 14:39:56 +0600

    in main

--- index.c
+++ index.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void primeNumber(){
-    printf("Function for        printing prime numbers");
+    printf("Function for printing prime numbers");
 }
 
 int main(){
@@ -9,6 +9,7 @@ int main(){
     printf("INDEX\n");
     printf("This is a new feature (dropdown) \n");
     printf("This is a new feature (button) \n");
+    printf("in main branch insertion");
 
     return 0;
 }
